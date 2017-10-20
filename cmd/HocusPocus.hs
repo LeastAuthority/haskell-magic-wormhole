855e5a9d54d73d7e669a8b8418c875c161050bac
Jonathan Lange <jml@mumak.net>
2017-10-20 09:01:11 +0100

    Wire up command-line parsing

--- cmd/HocusPocus.hs
+++ cmd/HocusPocus.hs
@@ -6,5 +6,45 @@ module Main (main) where
 
 import Protolude
 
+import Options.Applicative
+  ( Parser
+  , ParserInfo
+  , command
+  , execParser
+  , header
+  , helper
+  , hsubparser
+  , fullDesc
+  , info
+  , progDesc
+  )
+
+
+newtype Options
+  = Options
+  { cmd :: Command
+  } deriving (Eq, Show)
+
+optionsParser :: Parser Options
+optionsParser = Options <$> commandParser
+
+data Command
+  = Send
+  | Receive
+  deriving (Eq, Show)
+
+commandParser :: Parser Command
+commandParser = hsubparser $
+  command "send" (info (pure Send) (progDesc "Send a text message, file, or directory")) <>
+  command "receive" (info (pure Receive) (progDesc "Receive a text message, file, or directory"))
+
+makeOptions :: Text -> Parser a -> ParserInfo a
+makeOptions headerText parser = info (helper <*> parser) (fullDesc <> header (toS headerText))
+
 main :: IO ()
-main = putStrLn @Text "Hello world"
+main = do
+  options <- execParser (makeOptions "hocus-pocus - summon and traverse magic wormholes" optionsParser)
+  case cmd options of
+    -- XXX: What's the Protolude way of saying "print this as text"?
+    Send -> putStrLn @Text "send"
+    Receive -> putStrLn @Text "receive"
