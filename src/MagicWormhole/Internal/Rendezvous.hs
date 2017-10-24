b392031ad0c5477b8ddb1a474adcecc549f8fa6d
Jonathan Lange <jml@mumak.net>
2017-10-24 13:09:30 +0100

    Split client & server messages

--- src/MagicWormhole/Internal/Rendezvous.hs
+++ src/MagicWormhole/Internal/Rendezvous.hs
@@ -5,7 +5,8 @@
 -- import qualified MagicWormhole.Internal.Rendezvous as Rendezvous
 -- ```
 module MagicWormhole.Internal.Rendezvous
-  ( Message(..)
+  ( ClientMessage(..)
+  , ServerMessage(..)
   , receive
   , rpc
   , Connection
@@ -40,8 +41,6 @@ import MagicWormhole.Internal.WebSockets (WebSocketEndpoint(..))
 --
 -- Error messages
 -- * welcome messages can include 'error'
--- * there's a general 'error' type (indicated by 'type': 'error' key)
---   which also includes an 'orig' message
 --
 -- Some open questions:
 -- * general message stuff--how are we going to model this?
@@ -50,39 +49,51 @@ import MagicWormhole.Internal.WebSockets (WebSocketEndpoint(..))
 --   * messages from the server include 'server_tx', a float timestamp recording
 --     when the server received the message
 --   * messages from the server that are direct responses include a 'server_rx'
---     timestamp
+--     timestamp--unclear what this means?
 --   * do we want a separate Haskell type for each message type? e.g. PingMessage
 --   * if we do that, how do associate request/response pairs? e.g. PingMessage &
 --     PongMessage?
---   * do we want to have different types for messages from server (e.g. Ack,
---     Welcome, Pong) vs messages from client (e.g. Ping, Bind)?
 --   * do we want to (can we even?) structurally distinguish between messages that
 --     make sense outside the scope of a binding (e.g. ping) and messages that only
 --     make sense after a bind (e.g. allocate)
-data Message
+data ServerMessage
   = Welcome
-  | Ping Int
   | Pong Int
-  | Error { _errorMessage :: Text , _original :: Message }
+  | Error { _errorMessage :: Text , _original :: ClientMessage }
   | Ack
   deriving (Eq, Show)
 
-instance FromJSON Message where
+instance FromJSON ServerMessage where
   parseJSON (Object v) = do
     t <- v .: "type"
     case t of
       "welcome" -> pure Welcome
-      "ping" -> Ping <$> v .: "ping"
       "pong" -> Pong <$> v .: "pong"
       "ack" -> pure Ack
       "error" -> Error <$> v .: "error" <*> v .: "orig"
       _ -> fail $ "Unrecognized wormhole message type: " <> t
   parseJSON unknown = typeMismatch "Message" unknown
 
-instance ToJSON Message where
-  toJSON (Ping n) = object ["type" .= ("ping" :: Text), "ping" .= n]
+instance ToJSON ServerMessage where
   toJSON (Pong n) = object ["type" .= ("pong" :: Text), "pong" .= n]
   toJSON _ = notImplemented
+
+-- | A message sent from a rendezvous client to the server.
+data ClientMessage
+  = Ping Int
+  deriving (Eq, Show)
+
+instance FromJSON ClientMessage where
+  parseJSON (Object v) = do
+    t <- v .: "type"
+    case t of
+      "ping" -> Ping <$> v .: "ping"
+      _ -> fail $ "Unrecognized rendezvous client message type: " <> t
+  parseJSON unknown = typeMismatch "Message" unknown
+
+instance ToJSON ClientMessage where
+  toJSON (Ping n) = object ["type" .= ("ping" :: Text), "ping" .= n]
+
 
 type ParseError = String
 
@@ -93,17 +104,17 @@ newtype Connection = Conn { wsConn :: WS.Connection }
 -- Returns an error string if we cannot parse the message as a valid wormhole 'Message'.
 -- Throws exceptions if the underlying connection is closed or there is some error at the
 -- websocket level.
-receive :: Connection -> IO (Either ParseError Message)
+receive :: Connection -> IO (Either ParseError ServerMessage)
 receive = map eitherDecode . WS.receiveData . wsConn
 
 -- | Send a wormhole message to a websocket. Blocks until message is sent.
 -- Throws exceptions if the underlying connection is closed or there is some error at the
 -- websocket level.
-send :: Connection -> Message -> IO ()
+send :: Connection -> ClientMessage -> IO ()
 send (Conn conn) message = WS.sendBinaryData conn (encode message)
 
 -- | Make a request to the rendezvous server.
-rpc :: Connection -> Message -> IO (Either ParseError Message)
+rpc :: Connection -> ClientMessage -> IO (Either ParseError ServerMessage)
 rpc conn req = do
   send conn req
   -- XXX: Broken, because messages might arrive out of order.
