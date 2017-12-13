b772c7aace621aabee1ca59fa348da96ed53583f
Jonathan Lange <jml@mumak.net>
2017-12-13 21:04:47 +0000

    A test for sequential

--- tests/Tasty.hs
+++ tests/Tasty.hs
@@ -7,6 +7,7 @@ import Test.Tasty (defaultMain, testGroup)
 import qualified ClientProtocol
 import qualified Integration
 import qualified Messages
+import qualified Sequential
 import qualified WebSockets
 
 main :: IO ()
@@ -15,6 +16,7 @@ main = sequence tests >>= defaultMain . testGroup "MagicWormhole"
     tests =
       [ ClientProtocol.tests
       , Messages.tests
+      , Sequential.tests
       , WebSockets.tests
         -- Put these at the end because they are slowest.
       , Integration.tests
