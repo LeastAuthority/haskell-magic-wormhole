9237c4d242c785d18cc522474a1828855fdc8cb5
meejah <meejah@meejah.ca>
2023-05-31 23:09:55 -0600

    WIP: forgot this part

---
+++ Main.hs
@@ -0,0 +1,26 @@
+
+import qualified MagicWormhole.Internal.Versions as Versions
+import Data.Aeson.Types
+import Data.String (String)
+
+data InvitesVersion0 = InvitesVersion0 deriving (Eq, Show)
+
+instance ToJSON InvitesVersion0 where
+  toJSON InvitesVersion0 =
+    object ["magic-folder" .= object ["supported-messages" .= ["invite-v1" :: String]]]
+
+
+{-
+app_versions={
+     "magic-folder": {
+            "supported-messages": [
+                "invite-v1",
+            ],
+        },
+}
+-}
+
+
+main = do
+  let a = InvitesVersion0
+  print $ toJSON a
